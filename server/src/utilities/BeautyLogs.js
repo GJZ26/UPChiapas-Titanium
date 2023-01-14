65584a9e253ffefc58627964a1e0e520461924ed
GJZ26 <adolfogj26@gmail.com>
2023-01-13 23:12:28 -0600

    feat: adding new font color to BeautyLogs.js

--- server/src/utilities/BeautyLogs.js
+++ server/src/utilities/BeautyLogs.js
@@ -1,6 +1,12 @@
 const font_colors = {
     "yellow": "\x1b[33m",
     "blue": "\x1b[34m",
+    "black":"\x1b[30m",
+    "red":"\x1b[31m",
+    "green":"\x1b[32m",
+    "magenta":"\x1b[35m",
+    "cyan":"\x1b[36m",
+    "white":"\x1b[37m",
     "default": ""
 }
 
