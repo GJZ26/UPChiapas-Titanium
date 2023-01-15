caf089d109d0b92f08862c42fcb42cd467690dc1
GJZ26 <adolfogj26@gmail.com>
2023-01-14 23:08:33 -0600

    feat: Adding background support to colors and styles fonts to BeautyLogs utility script

--- server/src/utilities/BeautyLogs.js
+++ server/src/utilities/BeautyLogs.js
@@ -1,9 +1,9 @@
 const font_colors = {
-    "yellow": "\x1b[33m",
-    "blue": "\x1b[34m",
     "black":"\x1b[30m",
     "red":"\x1b[31m",
     "green":"\x1b[32m",
+    "yellow": "\x1b[33m",
+    "blue": "\x1b[34m",
     "magenta":"\x1b[35m",
     "cyan":"\x1b[36m",
     "white":"\x1b[37m",
@@ -11,13 +11,25 @@ const font_colors = {
 }
 
 const font_styles = {
-    "bold": "\x1b[1m",
     "reset": "\x1b[0m",
+    "bold": "\x1b[1m",
+    "dim": "\x1b[2m",
+    "underscore": "\x1b[4m",
+    "reverse": "\x1b[7m",
+    "hidden": "\x1b[8m",
     "default": ""
 }
 
 const bg_colors = {
-    "default": ""
+    "black": "\x1b[40m",
+    "red": "\x1b[41m",
+    "green": "\x1b[42m",
+    "yellow": "\x1b[43m",
+    "blue": "\x1b[44m",
+    "magenta": "\x1b[45m",
+    "cyan": "\x1b[46m",
+    "white": "\x1b[47m",
+    "default": "",
 }
 
 /**
